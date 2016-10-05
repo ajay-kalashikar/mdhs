44dd77155c9984accc3a033672173e5f155a07da
vivek bharos <vivek.bharos@connvertex.com>
2016-10-05 10:18:11 +0530

    change in directory structure

--- backend/modules/CareProviders/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'careproviders', 'namespace' => 'Modules\CareProviders\Http\Controllers'], function()
-{
-    Route::get('/', 'CareProvidersController@index');
-});
--- backend/modules/CareProviders/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('careproviders::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('careproviders.name') !!}
-    </p>
-@stop
